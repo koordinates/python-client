679684e5c2301f04e9be31e44fe96c3a35f02629
Richard Shea <rshea@thecubagroup.com>
2015-05-21 08:58:06 +1200

    Tidy up location of mega-strings used for testing
    There are a number of large strings used to simulate responses
    from the server and this change is just to get them outside
    of the main test code to make it easier to read.

--- tests/test_koordinates.py
+++ tests/test_koordinates.py
@@ -27,7 +27,7 @@ import koordinates
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
 from canned_responses_for_tests import layers_multiple_good_simulated_response
-#from canned_responses_for_tests import layers_single_good_simulated_response
+from canned_responses_for_tests import layers_single_good_simulated_response
 
 def getpass():
     '''
@@ -58,29 +58,6 @@ class TestKoordinates(unittest.TestCase):
     def test_layers_url_template(self):
         L = koordinates.api.Layer(self.koordconn, 999)
         assert L.url_templates('GET', 'single') == '''https://koordinates.com/services/api/v1/layers/{layer_id}/'''
-
-    @responses.activate
-    def test_basic_auth_1(self):
-        the_url = '''http://httpbin.org/basic-auth/user/passwd'''
-        the_response = '''
-            {
-              "authenticated": true, 
-              "user": "user"
-            }
-        '''
-        import json
-        the_response = json.dumps({'authenticated':True, 'user':'user'})
-        the_response.replace('\'' , '"')
-
-        responses.add(responses.GET, the_url,  
-                      body=the_response, status=200,
-                      content_type='application/json')
-
-        resp = requests.get(the_url)
-
-        assert len(responses.calls) == 1
-        assert responses.calls[0].request.url == the_url 
-        assert responses.calls[0].response.text == the_response
 
     @responses.activate
     def test_get_layerset_bad_auth(self):
@@ -126,7 +103,7 @@ class TestKoordinates(unittest.TestCase):
     @responses.activate
     def test_get_layer_by_id(self, id=1474):
 
-        the_response = '''{"id": 1474, "url": "https://koordinates.com/services/api/v1/layers/1474/", "type": "layer", "name": "Wellington City Building Footprints", "first_published_at": "2010-06-21T05:05:05.953", "published_at": "2012-05-09T02:11:27.020Z", "description": "Polygons representing building rooftop outlines for urban Wellington including Makara Beach and Makara Village. Each building has an associated elevation above MSL (Wellington 1953). The rooftop elevation does not include above roof structures such as aerials or chimneys. Captured in 1996 and updated in 1998, 1999, 2002, 2006, 2009, 2011 and 2012 in conjunction with aerial photography refly projects.", "description_html": "<p>Polygons representing building rooftop outlines for urban Wellington including Makara Beach and Makara Village. Each building has an associated elevation above MSL (Wellington 1953). The rooftop elevation does not include above roof structures such as aerials or chimneys. Captured in 1996 and updated in 1998, 1999, 2002, 2006, 2009, 2011 and 2012 in conjunction with aerial photography refly projects.</p>", "group": {"id": 119, "url": "https://koordinates.com/services/api/v1/groups/119/", "name": "Wellington City Council", "country": "NZ"}, "data": {"encoding": null, "crs": "EPSG:2193", "primary_key_fields": [], "datasources": [{"id": 65935}], "geometry_field": "GEOMETRY", "fields": [{"name": "GEOMETRY", "type": "geometry"}, {"name": "OBJECTID", "type": "integer"}, {"name": "Shape_Leng", "type": "double"}, {"name": "Shape_Area", "type": "double"}, {"name": "elevation", "type": "double"}, {"name": "feat_code", "type": "string"}, {"name": "source", "type": "string"}]}, "url_html": "https://koordinates.com/layer/1474-wellington-city-building-footprints/", "published_version": "https://koordinates.com/services/api/v1/layers/1474/versions/4067/", "latest_version": "https://koordinates.com/services/api/v1/layers/1474/versions/4067/", "this_version": "https://koordinates.com/services/api/v1/layers/1474/versions/4067/", "kind": "vector", "categories": [{"name": "Cadastral & Property", "slug": "cadastral"}], "tags": ["building", "footprint", "outline", "structure"], "collected_at": ["1996-12-31", "2012-05-01"], "created_at": "2010-06-21T05:05:05.953", "license": {"id": 9, "title": "Creative Commons Attribution 3.0 New Zealand", "type": "cc-by", "jurisdiction": "nz", "version": "3.0", "url": "https://koordinates.com/services/api/v1/licenses/9/", "url_html": "https://koordinates.com/license/attribution-3-0-new-zealand/"}, "metadata": {"iso": "https://koordinates.com/services/api/v1/layers/1474/versions/4067/metadata/iso/", "dc": "https://koordinates.com/services/api/v1/layers/1474/versions/4067/metadata/dc/", "native": "https://koordinates.com/services/api/v1/layers/1474/versions/4067/metadata/"}, "elevation_field": "elevation"}'''
+        the_response = layers_single_good_simulated_response
 
         L = koordinates.api.Layer(self.koordconn, 999)
 
@@ -153,6 +130,29 @@ class TestKoordinates(unittest.TestCase):
         assert responses.calls[0].request.url == 'http://twitter.com/api/1/foobar'
         assert responses.calls[0].response.text == '{"error": "not found"}'
  
+    @responses.activate
+    def test_basic_auth_1(self):
+        the_url = '''http://httpbin.org/basic-auth/user/passwd'''
+        the_response = '''
+            {
+              "authenticated": true, 
+              "user": "user"
+            }
+        '''
+        import json
+        the_response = json.dumps({'authenticated':True, 'user':'user'})
+        the_response.replace('\'' , '"')
+
+        responses.add(responses.GET, the_url,  
+                      body=the_response, status=200,
+                      content_type='application/json')
+
+        resp = requests.get(the_url)
+
+        assert len(responses.calls) == 1
+        assert responses.calls[0].request.url == the_url 
+        assert responses.calls[0].response.text == the_response
+
     def tearDown(self):
         pass
 
