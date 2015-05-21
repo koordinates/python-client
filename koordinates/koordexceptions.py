61fa64661be68d47fa893e8ffd61f53998ba31de
Richard Shea <rshea@thecubagroup.com>
2015-05-22 11:01:19 +1200

    Koord exceptions (meant to be in last commit)
    Some Koord Client specific exceptions.

---
+++ koordinates/koordexceptions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+class KoordinatesInvalidURL(Exception):
+    pass
+class KoordinatesNotAuthorised(Exception):
+    pass
+class KoordinatesUnexpectedServerResponse(Exception):
+    pass
