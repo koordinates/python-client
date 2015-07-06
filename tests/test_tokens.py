4df660704656f49483ebea2d49a260dd54dd7b0d
Richard Shea <rshea@thecubagroup.com>
2015-07-06 13:34:13 +1200

    Token PUT test

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -6,6 +6,7 @@ from koordinates import Token, Connection
 
 from response_data.responses_10 import token_get_multiple_tokens_good_response
 from response_data.responses_10 import token_get_single_token_good_response
+from response_data.responses_10 import token_good_create
 
 
 class TestTokens(unittest.TestCase):
@@ -54,3 +55,29 @@ class TestTokens(unittest.TestCase):
         self.assertEqual(obj_tok.created_at.year, 2015)
         self.assertEqual(obj_tok.created_at.hour, 5)
         self.assertEqual(obj_tok.expires_at, None)
+
+
+    @responses.activate
+    def test_create_token_good(self):
+        the_response = token_good_create
+
+        responses.add(responses.POST,
+                      self.conn.get_url('TOKEN', 'POST', 'create'),
+                      body=the_response, status=200,
+                      content_type='application/json')
+
+        obj_tok = Token(name='Sample Token for Testing')
+        obj_tok.scope = "query tiles catalog wxs:wfs wxs:wms wxs:wcs documents:read documents:write layers:read layers:write sets:read sets:write sources:read sources:write users:read users:write tokens:read tokens:write"
+        obj_tok.expires_at = '2015-08-01T08:00:00Z'
+        obj_tok_new = self.conn.tokens.create(obj_tok, 'foo@example.com', 'foobar')
+
+        self.assert_(isinstance(obj_tok_new, Token))
+
+        self.assertEqual(obj_tok_new.name, "Sample Token for Testing")
+        self.assertEqual(obj_tok_new.key_hint, "7fooxx...")
+        self.assertEqual(obj_tok_new.url, "https://test.koordinates.com/services/api/v1/tokens/987659/")
+        self.assertEqual(obj_tok_new.created_at.year, 2015)
+        self.assertEqual(obj_tok_new.created_at.hour, 1)
+        self.assertEqual(obj_tok_new.expires_at.year, 2015)
+        self.assertEqual(obj_tok_new.expires_at.hour, 8)
+        self.assertEqual(obj_tok_new.key, '77777777777777777777777777777777')
