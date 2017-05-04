751c81805f90bab60105656d33a602e821af96dc
Hamish Campbell <hn.campbell@gmail.com>
2017-05-05 10:13:32 +1200

    Version 0.3.0 release

--- koordinates/__init__.py
+++ koordinates/__init__.py
@@ -6,7 +6,7 @@ Koordinates Python API Client Library
 :license: BSD, see LICENSE for more details.
 '''
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 from .exceptions import (
     KoordinatesException,
