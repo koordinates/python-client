6cfe5e68dc97278d9cd139ee87d7b1b990a93efc
Craig de Stigter <craig@destigter.nz>
2021-07-30 11:49:26 +1200

    Drop python 3.4

--- setup.py
+++ setup.py
@@ -20,7 +20,9 @@ with open("README.md", "r", "utf-8") as f:
 
 setup(
     name="koordinates",
-    packages=["koordinates",],
+    packages=[
+        "koordinates",
+    ],
     version=version,
     description="A Python client library for a number of Koordinates web APIs",
     long_description=readme,
@@ -36,20 +38,24 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Scientific/Engineering :: GIS",
     ],
-    python_requires=">=3.4",
+    python_requires=">=3.5",
     install_requires=[
         "python-dateutil>=2,<3",
         "pytz",
         "requests>=2.5,<3",
         "requests-toolbelt>=0.4,<1",
     ],
-    tests_require=["pytest>=3.3", "responses>=0.3", "coverage>=3.7,<4",],
+    tests_require=[
+        "pytest>=3.3",
+        "responses>=0.3",
+        "coverage>=3.7,<4",
+    ],
     zip_safe=False,
 )
