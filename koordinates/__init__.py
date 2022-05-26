30234a8f47d7c00b0dd69da977d2c85531cf21d2
Hamish Campbell <hn.campbell@gmail.com>
2022-05-26 16:13:19 +1200

    Tag 0.6.0

--- koordinates/__init__.py
+++ koordinates/__init__.py
@@ -6,7 +6,7 @@ Koordinates Python API Client Library
 :license: BSD, see LICENSE for more details.
 """
 
-__version__ = "0.6.dev0"
+__version__ = "0.6.0"
 
 from .exceptions import (
     KoordinatesException,
